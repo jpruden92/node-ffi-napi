4962da61be20174d7c37e1ac0de6c9a4677f1159
Jorge Prudencio <jorge.prudencio@sngular.team>
2020-01-29 00:18:25 +0800

    Add build

---
+++ build/Release/.deps/Release/obj.target/ffi_bindings/src/callback_info.o.d
@@ -0,0 +1,31 @@
+cmd_Release/obj.target/ffi_bindings/src/callback_info.o := g++ '-DNODE_GYP_MODULE_NAME=ffi_bindings' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/opt/app-root/src/.node-gyp/10.16.3/include/node -I/opt/app-root/src/.node-gyp/10.16.3/src -I/opt/app-root/src/.node-gyp/10.16.3/deps/openssl/config -I/opt/app-root/src/.node-gyp/10.16.3/deps/openssl/openssl/include -I/opt/app-root/src/.node-gyp/10.16.3/deps/uv/include -I/opt/app-root/src/.node-gyp/10.16.3/deps/zlib -I/opt/app-root/src/.node-gyp/10.16.3/deps/v8/include -I/opt/app-root/src/node_modules/node-addon-api -I/opt/app-root/src/node_modules/get-uv-event-loop-napi-h/include -I/opt/app-root/src/node_modules/get-symbol-from-current-process-h/include -I../deps/libffi/include -I../deps/libffi/config/linux/x64  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/ffi_bindings/src/callback_info.o.d.raw   -c -o Release/obj.target/ffi_bindings/src/callback_info.o ../src/callback_info.cc
+Release/obj.target/ffi_bindings/src/callback_info.o: \
+ ../src/callback_info.cc ../src/ffi.h \
+ ../deps/libffi/config/linux/x64/ffi.h \
+ ../deps/libffi/config/linux/x64/ffitarget.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/uv.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/uv/errno.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/uv/version.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/uv/unix.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/uv/threadpool.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/uv/linux.h \
+ /opt/app-root/src/node_modules/node-addon-api/napi.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/node_api.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/node_api_types.h \
+ /opt/app-root/src/node_modules/node-addon-api/napi-inl.h \
+ /opt/app-root/src/node_modules/node-addon-api/napi-inl.deprecated.h
+../src/callback_info.cc:
+../src/ffi.h:
+../deps/libffi/config/linux/x64/ffi.h:
+../deps/libffi/config/linux/x64/ffitarget.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/uv.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/uv/errno.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/uv/version.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/uv/unix.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/uv/threadpool.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/uv/linux.h:
+/opt/app-root/src/node_modules/node-addon-api/napi.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/node_api.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/node_api_types.h:
+/opt/app-root/src/node_modules/node-addon-api/napi-inl.h:
+/opt/app-root/src/node_modules/node-addon-api/napi-inl.deprecated.h:
